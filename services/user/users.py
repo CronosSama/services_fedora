ba745cf6a4ae43377230650085d8168878cf03d3
Senpai <Senpai@node>
2021-07-05 13:32:19 +0100

    changed User but its honest work

--- services/user/users.py
+++ services/user/users.py
@@ -1,21 +1,24 @@
 import random
 import string
 import subprocess
-
+from multiprocessing import Pool
+import time
 class USSER():
   def __init__(self) :
       self.groupNumber = 0
       self.userNumber = 0
+      self.passwordLength = 0
       self.groupsName = []
       self.usernames = []
-      self.nmbrUsersInGroup = {}
+      self.All_Group_Info = []
+      self.arrayOfNumber = []
       self.CALLER()
 
-      
+
   def create_groups(self):
     print("we are in CREATE_GROUPE stage !!")
     for group in self.groupsName :
-      self.cmd(f"groupadd {group}")
+      
       print(f"group {group} has been created successfully !!!")
       
 
@@ -23,37 +26,53 @@ class USSER():
   def informations(self) :
     self.groupNumber = int(input("How many group you want to create ? "))
     self.userNumber = int(input("How many users you want to create ? : "))
+    self.passwordLength = int(input("how many letter you want to be in the password ? : "))
     for gn in range(self.groupNumber) : 
       self.groupsName.append(input(f"what the name of the group number {gn+1} : "))
-    
+    count = 0
     for grpName in self.groupsName :
-      self.nmbrUsersInGroup[grpName] = int(input(f"how many user will be in the {grpName} group ? : "))
+      nmb = int(input(f"how many user will be in the {grpName} group ? : "))
+      obj = {}
+      obj["name"] = grpName
+      obj["unum"] = nmb
+      obj["starter"] = count
+      self.All_Group_Info.append(obj)
+      count += nmb
+    print(self.All_Group_Info)
 
-    print(self.nmbrUsersInGroup)
 
+  def addUsersToGroup(self,One_Group_INFO,c) :
+    group = One_Group_INFO["name"]
+    
+    print(f"we are in the ADD_USER stage for group : { group } !!! ")
 
-  def addUsersToGroup(self) :
-    print("we are in the ADD_USER stage !!! ")
-    N = int(input("how many letter you want to be in the password ? : "))
-    with open("./passwd.txt","r+") as file :
-
-      i = 0
-      for group,nmbr in self.nmbrUsersInGroup.items() :
-        for _ in range(nmbr) :
-            password = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
-            username = f"user{i}"
-            full_user = f"{username}:{password}:{group}"
-            self.cmd(f"useradd -G {group} {username} ")
-            self.cmd(f"echo {username}:{password} | chpasswd ")
-            file.write(full_user+"\n")
-            print(f"user {username} has been added SUCCESSFULLY !!!")
-            i += 1
+    self.cmd(f"touch ./groups/{group}.txt")
+    self.cmd(f"groupadd {group}")
+    with open(f"./groups/{group}.txt","r+") as file :
+      i = One_Group_INFO["starter"]
+      # for One_Group_INFO in self.All_Group_Info :
+      for _ in range(One_Group_INFO["unum"]) :
+          password = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(self.passwordLength))
+          username = f"user{i}"
+          full_user = f"{username}:{password}:{group}"
+          self.cmd(f"sudo useradd -G {group} {username} ;  chpasswd {username}:{password} ")
+          file.write(full_user+"\n")
+          print(f"user {username} has been added SUCCESSFULLY !!!")
+          i += 1
 
   def CALLER(self) :
     self.informations()
-    self.create_groups()
-    self.addUsersToGroup()
-
+    self.cmd(f"mkdir ./groups") 
+    pool = Pool(processes=len(self.All_Group_Info))
+    start = time.time()
+    for c,One_Group_INFO in enumerate(self.All_Group_Info) : 
+      process = pool.apply_async(self.addUsersToGroup,[One_Group_INFO,c])
+      print("wait i'am finished already ? wtf ")
+    pool.close()
+    pool.join() 
+      
+    end = time.time()
+    print(f"it tooks {end - start} to whole things done .... ")
   def cmd(self,command):
     subprocess.run(command,shell=True)
 
--- test.py
+++ test.py
@@ -1,4 +1,36 @@
 import re
+import time 
+from threading import Thread
+from multiprocessing import Pool
+
+def func(n):
+  print(f"func {n} : STARTING ....")
+  d = 0
+  while d < 500000000:
+    d += 1
+  print(f"func {n} : FINISHED ....")
+
+# t_1 = Thread(target=func,args=([1]))
+# t_2 = Thread(target=func,args=([2]))
+pool = Pool(processes=2)
+start = time.time()
+print("i started here")
+
+
+r1 = pool.apply_async(func,[1])
+r1 = pool.apply_async(func,[2])
+pool.close()
+pool.join()
+# t_1.start()
+# t_2.start()
+# t_1.join()
+# t_2.join()
+finish = time.time()
+
+print(f"finished in : { finish - start } ")
+
+
+
 
 # with open("./Config/dhcp/try.txt","r") as file :
 #   origin = file.readlines()
@@ -12,10 +44,10 @@ import re
 #       break
 
 # print(the_Ligne_number)
-with open("./passwd.txt","r") as file :
-  origin = file.readlines()
+# with open("./passwd.txt","r") as file :
+#   origin = file.readlines()
 
-for i in range(len(origin)-1) :
-  origin[i] = origin[i].strip() + f":group"
-  print(origin[i].split(":")[0])
-print(len(origin))
+# for i in range(len(origin)-1) :
+#   origin[i] = origin[i].strip() + f":group"
+#   print(origin[i].split(":")[0])
+# print(len(origin))
